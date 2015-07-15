ee35cbf8748adaa079d42df179510e77f14d8b80
Rik Scarborough <riksca@gmail.com>
2015-07-15 15:10:36 -0500

    Update dependencies to remove files api reference.

--- adminb/src/main/groovy/groovy/BackupData.groovy
+++ adminb/src/main/groovy/groovy/BackupData.groovy
@@ -7,6 +7,7 @@ import com.google.appengine.api.blobstore.BlobKey
 import com.google.appengine.api.datastore.*
 import static com.google.appengine.api.datastore.FetchOptions.Builder.*
 import com.google.appengine.api.ThreadManager
+import java.io.*
 
 
 def runToday = false
@@ -36,230 +37,229 @@ if(runToday) {
 }
 
 Thread thread = ThreadManager.createBackgroundThread(new Runnable() {
-    public void run() {
+        public void run() {
 
-        Security security = SecurityFactory.getSecurity()
+            Security security = SecurityFactory.getSecurity()
 
-        def fighterCount = datastore.execute {
-            select count from Fighter
-        }
-
-        logger.StoreDatabase.info "Count returns " + fighterCount
-
-        def loopTimes = fighterCount.div(limitNum).intValue() + 1
-
-        logger.StoreDatabase.info "Looping " + loopTimes + " times"
-
-        def fighterIndex = search.index("fighters")
-
-        def mapList = []
-        long savedCount = 0L
-        for (i in 0..loopTimes) {
-
-            def fighters = datastore.iterate {
-                select all from Fighter
-                prefetchSize fighterCount
-                chunkSize limitNum
-                limit limitNum
-                offset i == 0 ? 0 : limitNum * i
-                restart automatically
+            def fighterCount = datastore.execute {
+                select count from Fighter
             }
 
-            for (fighter in fighters) {
-                def response = fighterIndex.put {
-                    document(id: fighter.key.id) {
-                        scaName text: fighter.scaName
-                        modernName text: fighter.modernName
-                        googeId text: fighter.googleId
+            logger.StoreDatabase.info "Count returns " + fighterCount
+
+            def loopTimes = fighterCount.div(limitNum).intValue() + 1
+
+            logger.StoreDatabase.info "Looping " + loopTimes + " times"
+
+            def fighterIndex = search.index("fighters")
+
+            def mapList = []
+            long savedCount = 0L
+            for (i in 0..loopTimes) {
+
+                def fighters = datastore.iterate {
+                    select all from Fighter
+                    prefetchSize fighterCount
+                    chunkSize limitNum
+                    limit limitNum
+                    offset i == 0 ? 0 : limitNum * i
+                    restart automatically
+                }
+
+                for (fighter in fighters) {
+                    def response = fighterIndex.put {
+                        document(id: fighter.key.id) {
+                            scaName text: fighter.scaName
+                            modernName text: fighter.modernName
+                            googeId text: fighter.googleId
+                        }
+                    }
+                    def fmap = [:]
+                    fmap.fighterId = fighter.key.id
+                    fmap.scaName = fighter.scaName
+                    fmap.scaMemberNo = fighter.scaMemberNo
+                    fmap.modernName = fighter.modernName
+                    fmap.dateOfBirth = fighter.dateOfBirth
+                    fmap.googleId = fighter.googleId
+                    def emailList = []
+                    def emails = datastore.execute {
+                        select all from 'Email'
+                        ancestor fighter.key
+                    }
+                    emails.each {email ->
+                        def emailMap = [:]
+                        emailMap.emailAddress = email.emailAddress
+                        emailMap.type = email.type
+                        emailList << emailMap
+                    }
+                    fmap.email = emailList
+                    def addresses = datastore.execute {
+                        select all from Address
+                        ancestor fighter.key
+                    }
+                    def addressList = []
+                    addresses.each { address->
+                        def addressMap = [:]
+                        addressMap.address1 = address.address1
+                        addressMap.address2 = address.address2
+                        addressMap.city = address.city
+                        addressMap.district = address.district
+                        addressMap.postalCode = address.postalCode
+                        addressMap.state = address.state
+                        addressMap.type = address.type
+                        addressList << addressMap
+                    }
+                    fmap.address = addressList
+                    def phones = datastore.execute {
+                        select all from Phone
+                        ancestor fighter.key
+                    }
+                    def phoneList = []
+                    phones.each {phone ->
+                        def phoneMap = [:]
+                        phoneMap.phoneNumber = phone.phoneNumber
+                        phoneMap.type = phone.type
+                        phoneList << phoneMap
+                    }
+                    fmap.phone = phoneList
+                    def authList = []
+                    def authorizations = datastore.execute {
+                        select all from Authorization
+                        ancestor fighter.key
+                    }
+                    authorizations.each { auth ->
+                        def authMap = [:]
+                        def authType = auth.authType.get()
+                        authMap.code = authType.code
+                        authMap.description = authType.description
+                        authMap.date = auth.date
+                        authMap.orderValue = authType.orderValue
+                        authList << authMap
+                    }
+                    fmap.authorization = authList
+                    if(fighter.scaGroup) {
+                        def group = fighter.scaGroup.get()
+                        fmap.group = group.groupName
+                    } else {
+                        fmap.group = "Unknown or Out of Kingdom"
+                    }
+                    fmap.role = fighter.role
+                    fmap.status = fighter.status
+                    fmap.treaty = fighter.treaty?.name
+                    mapList << fmap
+                    ++savedCount
+                }
+            }
+
+            logger.StoreDatabase.info "mapList size " + mapList.size()
+
+            if (savedCount == fighterCount) {
+                logger.BackupData.info "Saved " + savedCount + " fighters"
+            } else {
+                logger.BackupData.warning "Only found " + savedCount + " fighters. Ending"
+                return
+            }
+
+            def scaGroups = datastore.execute {
+                select all from 'ScaGroup' as ScaGroup
+            }
+
+            def groupsMap = [:]
+            scaGroups.each { group ->
+                groupsMap[group.groupName] = group
+            }
+
+            def reportIndex = search.index("reports")
+            def reportList = []
+            namespace.of("calontir") {
+                def reports = datastore.iterate {
+                    select all from 'Report'
+                    restart automatically
+                }
+                reports.each { r ->
+                    def params = datastore.execute {
+                        select all from 'ReportParams'
+                        where reportKey == r.key
+                    }
+                    def rmap = [:]
+                    rmap.id = r.key.id
+                    rmap.dateEntered = r.dateEntered
+                    rmap.reportType = r.reportType
+                    rmap.marshalName = r.marshalName
+                    rmap.marshalId = r.marshalId
+                    rmap.googleId = r.googleId
+                    def paramMap = [:]
+                    params.each {param ->
+                        paramMap[param.name] = param.value
+                    }
+                    rmap.reportParams = paramMap
+                    reportList << rmap
+                    def response = reportIndex.put {
+                        document(id: r.key.id) {
+                            marshalName text: r.marshalName
+                            reportType text: r.reportType
+                            googleId text: r.googleId
+                            group text: paramMap["Group"]
+                            region text : groupsMap[paramMap["Group"]]?.groupLocation
+                            dateEntered date : Date.parse("yyyy-MM-dd'T'HH:mm:ss", r.dateEntered)
+                        }
                     }
                 }
-                def fmap = [:]
-                fmap.fighterId = fighter.key.id
-                fmap.scaName = fighter.scaName
-                fmap.scaMemberNo = fighter.scaMemberNo
-                fmap.modernName = fighter.modernName
-                fmap.dateOfBirth = fighter.dateOfBirth
-                fmap.googleId = fighter.googleId
-                def emailList = []
-                def emails = datastore.execute {
-                    select all from 'Email'
-                    ancestor fighter.key
-                }
-                emails.each {email ->
-                    def emailMap = [:]
-                    emailMap.emailAddress = email.emailAddress
-                    emailMap.type = email.type
-                    emailList << emailMap
-                }
-                fmap.email = emailList
-                def addresses = datastore.execute {
-                    select all from Address
-                    ancestor fighter.key
-                }
-                def addressList = []
-                addresses.each { address->
-                    def addressMap = [:]
-                    addressMap.address1 = address.address1
-                    addressMap.address2 = address.address2
-                    addressMap.city = address.city
-                    addressMap.district = address.district
-                    addressMap.postalCode = address.postalCode
-                    addressMap.state = address.state
-                    addressMap.type = address.type
-                    addressList << addressMap
-                }
-                fmap.address = addressList
-                def phones = datastore.execute {
-                    select all from Phone
-                    ancestor fighter.key
-                }
-                def phoneList = []
-                phones.each {phone ->
-                    def phoneMap = [:]
-                    phoneMap.phoneNumber = phone.phoneNumber
-                    phoneMap.type = phone.type
-                    phoneList << phoneMap
-                }
-                fmap.phone = phoneList
-                def authList = []
-                def authorizations = datastore.execute {
-                    select all from Authorization
-                    ancestor fighter.key
-                }
-                authorizations.each { auth ->
-                    def authMap = [:]
-                    def authType = auth.authType.get()
-                    authMap.code = authType.code
-                    authMap.description = authType.description
-                    authMap.date = auth.date
-                    authMap.orderValue = authType.orderValue
-                    authList << authMap
-                }
-                fmap.authorization = authList
-                if(fighter.scaGroup) {
-                    def group = fighter.scaGroup.get()
-                    fmap.group = group.groupName
-                } else {
-                    fmap.group = "Unknown or Out of Kingdom"
-                }
-                fmap.role = fighter.role
-                fmap.status = fighter.status
-                fmap.treaty = fighter.treaty?.name
-                mapList << fmap
-                ++savedCount
             }
-        }
 
-        logger.StoreDatabase.info "mapList size " + mapList.size()
-
-        if (savedCount == fighterCount) {
-            logger.BackupData.info "Saved " + savedCount + " fighters"
-        } else {
-            logger.BackupData.warning "Only found " + savedCount + " fighters. Ending"
-            return
-        }
-
-        def scaGroups = datastore.execute {
-            select all from 'ScaGroup' as ScaGroup
-        }
-
-        def groupsMap = [:]
-        scaGroups.each { group ->
-            groupsMap[group.groupName] = group
-        }
-
-        def reportIndex = search.index("reports")
-        def reportList = []
-        namespace.of("calontir") {
-            def reports = datastore.iterate {
-                select all from 'Report'
-                restart automatically
+            def json = new groovy.json.JsonBuilder()
+            def now = new Date()
+            def root = json {
+                dateBackedUp String.format('%tF %<tT', now.time)
+                fighters mapList
+                reports reportList
             }
-            reports.each { r ->
-                def params = datastore.execute {
-                    select all from 'ReportParams'
-                    where reportKey == r.key
-                }
-                def rmap = [:]
-                rmap.id = r.key.id
-                rmap.dateEntered = r.dateEntered
-                rmap.reportType = r.reportType
-                rmap.marshalName = r.marshalName
-                rmap.marshalId = r.marshalId
-                rmap.googleId = r.googleId
-                def paramMap = [:]
-                params.each {param ->
-                    paramMap[param.name] = param.value
-                }
-                rmap.reportParams = paramMap
-                reportList << rmap
-                def response = reportIndex.put {
-                    document(id: r.key.id) {
-                        marshalName text: r.marshalName
-                        reportType text: r.reportType
-                        googleId text: r.googleId
-                        group text: paramMap["Group"]
-                        region text : groupsMap[paramMap["Group"]]?.groupLocation
-                        dateEntered date : Date.parse("yyyy-MM-dd'T'HH:mm:ss", r.dateEntered)
-                    }
-                }
-            }
-        }
 
-        def json = new groovy.json.JsonBuilder()
-        def now = new Date()
-        def root = json {
-            dateBackedUp String.format('%tF %<tT', now.time)
-            fighters mapList
-            reports reportList
-        }
-
-        def file = files.createNewBlobFile("text/json", String.format("backup%tY%tm%td.json", now, now, now))
-
-        file.withWriter {writer ->
+            //def file = files.createNewBlobFile("text/json", String.format("backup%tY%tm%td.json", now, now, now))
+            // we need to do somethign else with the json string.
+            def writer = new StringWriter()
             writer << json.toString()
-        }
 
-        namespace.of("system") {
-            def name = "calontir.backupkey"
-            def query = new Query("properties")
-            query.addFilter("name", Query.FilterOperator.EQUAL, name)
-            PreparedQuery preparedQuery = datastore.prepare(query)
-            def entities = preparedQuery.asList( withDefaults() )
-            entities.each {
-                it.delete()
+            namespace.of("system") {
+                def name = "calontir.backupkey"
+                def query = new Query("properties")
+                query.addFilter("name", Query.FilterOperator.EQUAL, name)
+                PreparedQuery preparedQuery = datastore.prepare(query)
+                def entities = preparedQuery.asList( withDefaults() )
+                entities.each {
+                    it.delete()
+                }
+
+                Entity sysTable = new Entity("properties")
+                sysTable.name = name
+                sysTable.property = file.blobKey.keyString
+
+                sysTable.save()
+
+                def lastbackupKey = "calontir.lastbackup"
+                query = new Query("properties")
+                query.addFilter("name", Query.FilterOperator.EQUAL, lastbackupKey)
+                preparedQuery = datastore.prepare(query)
+                entities = preparedQuery.asList( withDefaults() )
+                entities.each {
+                    it.delete()
+                }
+
+                sysTable = new Entity("properties")
+                sysTable.name = lastbackupKey
+                sysTable.property = new Date().time
+
+                sysTable.save()
             }
 
-            Entity sysTable = new Entity("properties")
-            sysTable.name = name
-            sysTable.property = file.blobKey.keyString
-
-            sysTable.save()
-
-            def lastbackupKey = "calontir.lastbackup"
-            query = new Query("properties")
-            query.addFilter("name", Query.FilterOperator.EQUAL, lastbackupKey)
-            preparedQuery = datastore.prepare(query)
-            entities = preparedQuery.asList( withDefaults() )
-            entities.each {
-                it.delete()
-            }
-
-            sysTable = new Entity("properties")
-            sysTable.name = lastbackupKey
-            sysTable.property = new Date().time
-
-            sysTable.save()
-        }
-
-        html.html {
-            body {
-                p "Done"
-                p file.toString()
-                p file.blobKey.keyString
+            html.html {
+                body {
+                    p "Done"
+                    p file.toString()
+                    p file.blobKey.keyString
+                }
             }
         }
-    }
-});
+    });
 thread.start()
 
