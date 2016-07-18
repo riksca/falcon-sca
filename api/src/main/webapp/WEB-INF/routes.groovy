aa104d5fb3ca82dab3d5cad08d581254fde35922
Rik Scarborough <riksca@gmail.com>
2016-07-18 15:16:55 -0500

    Adding API module.

--- api/src/main/webapp/WEB-INF/routes.groovy
+++
@@ -1,5 +0,0 @@
-get "/EarlMarshalReport", forward: "EarlMarshalReport.groovy"
-get "/QueueTask/@taskname", forward: "/WEB-INF/groovy/QueueTask.groovy?task=@taskname.groovy"
-// RESTful paths
-//post "/createFighter", forward: "/WEB-INF/groovy/post.groovy"
-//get "/fighter/@name", forward: "/WEB-INF/groovy/GetFighter.groovy?name=@name"
